171e1d94db6ec1a2a68038f9efe44bb1d8df3e4c
junyeopp <junyeop@sogang.ac.kr>
2020-03-30 15:40:15 +0900

    test

---
+++ 6.3.PICNIC.py
@@ -0,0 +1,16 @@
+f = open("./6.3.picnic_input.txt", 'r')
+#   lines = []
+#   i = 0
+#   while True:
+#       line = f.readline()
+#       if not line: break
+#       lines.append(line.split())
+#   f.close()
+
+
+c = f.readline()
+print(c.split())
+
+
+
+f.close()
